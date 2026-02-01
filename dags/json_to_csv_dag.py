bc983bd8cc1e6af91e99abefc1872be42125dbb1
Fedosz <fedya.zhukov.2014@mail.ru>
2026-02-01 14:05:10 +0300

    add solution

---
+++ dags/json_to_csv_dag.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import json
+import pandas as pd
+
+def csv():
+    with open("/opt/airflow/data/pets-data.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    df = pd.json_normalize(data)
+    df.to_csv("/opt/airflow/data/result.csv", index=False, encoding="utf-8")
+
+
+with DAG(dag_id="json_csv", start_date=datetime(2024, 1, 1), schedule_interval=None, catchup=False) as dag:
+    transform = PythonOperator(task_id="csv", python_callable=csv)
+    transform
